3ff030a5f9edfb19d6bd66ebf88aa40ee3ee716a
bobuojisann <70473365+bobuojisann@users.noreply.github.com>
2020-10-03 18:51:50 +0900

    ログアウト状態でも商品詳細ページが閲覧できる実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
-
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show ]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -34,5 +34,8 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :comment, :category_id, :product_condition_id, :delivery_fee_id, :area_id, :date_of_shipment_id, :price, :image).merge(user_id: current_user.id)
+  end
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
