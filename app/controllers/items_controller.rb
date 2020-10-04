0c480a42db077b9155e139cca7acfd8e7394df7a
bobuojisann <70473365+bobuojisann@users.noreply.github.com>
2020-10-04 16:45:10 +0900

    商品情報編集機能、実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show ]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.order('created_at DESC')
   end
@@ -22,12 +22,23 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    
   end
-
+ 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      rendre :show
+    end
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
