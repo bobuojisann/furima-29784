680f3cfa3f45a4b2fb2aa40bf44ba1173bebc02e
bobuojisann <70473365+bobuojisann@users.noreply.github.com>
2020-10-19 15:40:50 +0900

    エラーコード日本語変換記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,14 +13,14 @@ class Item < ApplicationRecord
     validates :name
     validates :comment
     validates :image
-    validates :price, format: { with: /\A[0-9]+\z/, message: 'Peice Half-width number' }
+    validates :price, format: { with: /\A[0-9]+\z/, message: 'を半角数字で入力してください' }
   end
-  with_options numericality: { other_than: 1 } do
+  with_options numericality: { other_than: 1, message: 'を入力してください' } do
     validates :category_id
     validates :product_condition_id
     validates :delivery_fee_id
     validates :area_id
     validates :date_of_shipment_id
   end
-  validates :price, numericality: { greater_than: 300, less_than_or_equal_to: 9_999_999, message: 'Price Out of setting range' }
+  validates :price, numericality: { greater_than: 300, less_than_or_equal_to: 9_999_999, message: 'を300~9,999,999の間で入力してください' }
 end
